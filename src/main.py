4761256b7ed147b5cc93af2537d36eb8231defbd
latiang <haraldbjurulf@hotmail.com>
2021-02-09 22:05:01 +0100

    Add work pre repo

---
+++ src/main.py
@@ -0,0 +1,34 @@
+import time
+import decimal
+import sys
+
+
+def main():
+    file = open("pi.txt")
+    lines = file.readlines()
+    print("File done")
+    decimal.getcontext().prec = 10**10
+    decimal.getcontext().Emin = -999999999999999
+    decimal.getcontext().Emax = 0-decimal.getcontext().Emin
+
+    nums = 10**8
+
+    start = time.time()
+    pi = lines[0][2:nums]
+    pi = decimal.Decimal(pi)
+    end = time.time()
+    print(sys.getsizeof(pi))
+    base = decimal.getcontext().power(2, 10 ** 8)
+    i = 0
+    start = time.time()
+    while pi > base:
+        print(i)
+        pi -= pi % base
+        pi //= base
+        i += 1
+    end = time.time()
+    print(end - start)
+    
+
+if __name__ == "__main__":
+    main()
