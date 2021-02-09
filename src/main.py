6d6251bd737157f4be41caa3e6194781622ce044
latiang <haraldbjurulf@hotmail.com>
2021-02-09 23:55:30 +0100

    Fix to binary converter

--- src/main.py
+++ src/main.py
@@ -1,18 +1,22 @@
 import time
 import decimal
 import sys
-import generate_image
-import comp_helper
+#import generate_image
+#import comp_helper
 
 
-def convert_base_2(num: decimal.Decimal, cur_pow: int):
+def convert_base_2(num: decimal.Decimal, cur_pow: int, lookup: dict):
     if cur_pow < 1:
         return [int(num)]
     else:
-        base = decimal.getcontext().power(2, cur_pow)
+        if cur_pow in lookup:
+            base = lookup[cur_pow]
+        else:
+            base = decimal.getcontext().power(2, cur_pow)
+            lookup[cur_pow] = base
         digits = []
         if num == 0:
-            digits = [0] * cur_pow
+            digits = [0]
             return digits
         while num:
             digit = num % base
@@ -20,7 +24,7 @@ def convert_base_2(num: decimal.Decimal, cur_pow: int):
             
             num = num // base
         dummy = 0
-        digits = [convert_base_2(n, cur_pow // 2) for n in digits]
+        digits = [convert_base_2(n, cur_pow // 2, lookup) for n in digits]
         digits_n = []
         for digit in digits:
             digits_n.extend(digit)
@@ -34,17 +38,27 @@ def main():
     file = open("pi.txt")
     lines = file.readlines()
     print("File done")
-    nums = 10**1
+    nums = 10**7
     pi = '3' + lines[0][2:nums]
     pi = decimal.Decimal(pi)
-    pi = decimal.Decimal(123)
-    start_pow = 5
-    print(pi)
-    binary = convert_base_2(pi, start_pow)
+    start_pow = 2**25
+    #print(pi)
 
+    print("start")
+    start = time.time()
+    binary = convert_base_2(pi, start_pow, dict())
+    end = time.time()
+    print("end: ", end - start)
+
+    #val = sum([val << i for i, val in enumerate(binary)])
+    #print(val)
+    #print(binary)
+    """
+    binary = convert_base_2(decimal.Decimal(48), 2**(2**3))
     val = sum([val << i for i, val in enumerate(binary)])
-    print(val)
     print(binary)
+    print(val)
+    print(48)"""
     
 
 if __name__ == "__main__":
