796bb25270109c6e968253c3c3c0d9242fd0add1
latiang <haraldbjurulf@hotmail.com>
2021-02-11 16:05:16 +0100

    Add proper binary expansion

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
 import time
 import decimal
 import sys
+import math
 #import generate_image
 #import comp_helper
+def to_hex(num: list):
+    symbols = '0123456789ABCDEF'
+    ind = 0
+    for i, val in enumerate(num):
+        ind = ind * 2 + val
+        if i % 4 == 3:
+            yield symbols[ind]
+            ind = 0
+    while i % 4 != 3:
+        i += 1
+        ind *= 2
+    yield symbols[ind]
 
+    
+
+def flip_number(num: list):
+    first_val_found = False
+    ret_val = []
+    for val in reversed(num):
+        if first_val_found:
+            ret_val.append(val)
+        elif val != 0:
+            ret_val.append(val)
+            first_val_found = True
+    return ret_val
 
 def convert_base_2(num: decimal.Decimal, cur_pow: int, lookup: dict):
     if cur_pow < 1:
@@ -32,33 +57,51 @@ def convert_base_2(num: decimal.Decimal, cur_pow: int, lookup: dict):
         return digits_n
 
 def main():
-    decimal.getcontext().prec = 10**10
+    nums = 10**6
+    decimal.getcontext().prec = 10**9
     decimal.getcontext().Emin = -999999999999999
     decimal.getcontext().Emax = 0-decimal.getcontext().Emin
     file = open("pi.txt")
     lines = file.readlines()
     print("File done")
-    nums = 10**7
-    pi = '3' + lines[0][2:nums]
-    pi = decimal.Decimal(pi)
-    start_pow = 2**25
-    #print(pi)
 
+    pi = '3' + lines[0][2:nums + 1]
+    pi = decimal.Decimal(pi)
+    print(sys.getsizeof(pi))
+    context = decimal.getcontext()
+    k = context.logb(pi)   #pi has been multiplied by 10^k (we removed the .)
+        #We want to remove the factor 5 ^ k in order to preserve binary order of digits
+        #To do this and preserve an integer we can work with we need to mulitply by 2 ^ m / 5 ^ k
+        #for some m such that 2 ^ m is of a greater magnitude than 5 ^ k
+    denum_part = context.power(5, k)
+    num_part = context.power(2, int(int(k) * math.log2(5)) + 1)
+    pi = (pi * num_part) // denum_part
+    
+    pi = decimal.Decimal(pi)
+    start_exp = int(math.log2(float(int(context.logb(pi)) * (1/math.log10(2)))))
+    del denum_part
+    del num_part
+    start_pow = 2**start_exp
+
+    print(context.logb(pi), k)
     print("start")
     start = time.time()
     binary = convert_base_2(pi, start_pow, dict())
+    binary = flip_number(binary)
     end = time.time()
     print("end: ", end - start)
+
+
+    hx_partion = binary[2:]
+    hx_partion = list(to_hex(hx_partion))
+    st = ''
+    for i in range(50):
+        st += hx_partion[10**6 - 50 + i]
+    print(st)
 
     #val = sum([val << i for i, val in enumerate(binary)])
     #print(val)
     #print(binary)
-    """
-    binary = convert_base_2(decimal.Decimal(48), 2**(2**3))
-    val = sum([val << i for i, val in enumerate(binary)])
-    print(binary)
-    print(val)
-    print(48)"""
     
 
 if __name__ == "__main__":
